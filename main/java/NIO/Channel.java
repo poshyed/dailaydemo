7b5c2be81e1fd27d7c1616a0b4df8e24f3b9075c
poshyed <413824546@qq.com>
2017-12-08 11:35:05 +0800

    filechannel

---
+++ main/java/NIO/Channel.java
@@ -0,0 +1,27 @@
+package NIO;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * Created by posy on 17.12.8.
+ */
+public class Channel {
+    public void fileRead(String fileName) throws IOException {
+        RandomAccessFile randomAccessFile = new RandomAccessFile(fileName, "rw");
+        FileChannel inChannel = randomAccessFile.getChannel();
+        ByteBuffer byteBuffer = ByteBuffer.allocate(48);
+        int byteRead = inChannel.read(byteBuffer);
+        while (byteRead != -1) {
+            System.out.println("Read" + byteRead);
+            byteBuffer.flip();
+            while (byteBuffer.remaining() == 0) {
+                System.out.println(byteBuffer.get());
+            }
+            byteBuffer.clear();
+            byteRead = inChannel.read(byteBuffer);
+        }
+    }
+}
